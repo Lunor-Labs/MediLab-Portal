0281eb8bccb7035a052e0b9696f3031e22f015e0
Imalkaaa <imalkadhananja28@gmail.com>
2025-06-15 10:39:10 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/medilab-portal-website/',
+    base: '/MediLab-Portal/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
